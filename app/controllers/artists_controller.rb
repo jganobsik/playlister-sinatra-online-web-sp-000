a1651e9262d2dd1b4aee18045e38d6a16265588b
Joe Ganobsik <jganobsik@gmail.com>
2019-04-01 16:35:28 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,13 +2,13 @@ class ArtistsController < ApplicationController
     get '/artists' do
       
     @artists = Artist.all
-    erb:'artists/index'
+    erb:'artists/index.html'
   end
 
   get '/artists/:slug' do
     
     @artist = Artist.find_by_slug(params[:slug])
-    erb :'artists/show'
+    erb :'artists/show.html'
   end
   
   
